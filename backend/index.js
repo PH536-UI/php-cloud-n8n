6f8e591ac57d53ebd4636ff90c79e4c5502f8367
PH536-UI <paulohenrique2020pereira@gmail.com>
2025-12-31 09:28:37 -0300

    fix: adiciona vercel.json e entry point

--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("PhP Cloud Backend OK"));
+app.get("/", (req, res) => res.send("PhP Cloud Backend - OK"));
+app.get("/create", (req, res) => res.send("Criar Workflow - Em construção"));
+
 app.listen(port, () => console.log(`Rodando na porta ${port}`));
